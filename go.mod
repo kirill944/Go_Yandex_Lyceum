f2f3d25a77580fa22c9dd9f3e62185c37b5c4a67
ksolo <k.solovev994@yandex.ru>
2025-03-04 13:02:03 +0300

    f

---
+++ go.mod
@@ -0,0 +1,5 @@
+module Calculation
+
+go 1.23.4
+
+require github.com/kirill944/Go_Yandex_Lyceum v0.0.0-20250218173520-bdc9b6dff4d0
--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "Go_Yandex_Lyceum/internal/application"
-
 func main() {
 	app := application.New()
 	app.RunServer()
